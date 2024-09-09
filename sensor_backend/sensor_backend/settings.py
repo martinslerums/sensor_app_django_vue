d272584afe94967f3761d1a1786441d55218703a
martleru <martins.lerums@hotmail.com>
2024-09-09 11:16:10 +0300

    Updated CORS

--- sensor_backend/sensor_backend/settings.py
+++ sensor_backend/sensor_backend/settings.py
@@ -20,10 +20,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -58,8 +58,10 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
+    'http://localhost:8080', 
+    'http://localhost:5173',  
 ]
+
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_HEADERS = [
     'content-type',
